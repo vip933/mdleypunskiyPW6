23932c140cad71a838fb339547090ef24ff585c6
vip933 <mdleypunskiy@edu.hse.ru>
2021-11-20 20:07:57 +0300

    1 point - created project and removed storyboard

---
+++ mdleypunskiyPW6/mdleypunskiyPW6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mdleypunskiyPW6
+//
+//  Created by Maksim on 20.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
