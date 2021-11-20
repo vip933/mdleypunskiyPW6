2f563103c887ec76bfa3a11edfbd4aa86c829567
vip933 <mdleypunskiy@edu.hse.ru>
2021-11-20 20:13:58 +0300

    2 point - added buttons with actions

--- mdleypunskiyPW6/mdleypunskiyPW6/ViewController.swift
+++ mdleypunskiyPW6/mdleypunskiyPW6/ViewController.swift
@@ -11,7 +11,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGray
+        setupButtons()
+    }
+    
+    var buttons: [UIButton] = []
+    let texts = ["Log (framework)", "Log (swift package)", "Log (pod)", "Log (carthage)"]
+    private func setupButtons() {
+        var top = view.frame.height / 2 - 90
+        for i in 0..<4 {
+            let button = UIButton()
+            view.addSubview(button)
+            button.setTitle(texts[i], for: .normal)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.topAnchor.constraint(equalTo: view.topAnchor, constant: CGFloat(top)).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 200).isActive = true
+            button.leadingAnchor.constraint(
+                equalTo: view.leadingAnchor,
+                constant: view.frame.width / 2 - 100
+            ).isActive = true
+            button.heightAnchor.constraint(equalToConstant: 30).isActive = true
+            self.buttons.append(button)
+            button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+            top += 40
+        }
+    }
+    
+    @objc
+    private func buttonPressed(sender: UIButton) {
+        print(sender.titleLabel?.text ?? "Error")
     }
 
 
